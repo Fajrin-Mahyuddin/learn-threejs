6a13a307c94561331908cb7a2d61f00d41ab92e9
fajrin <fajrin.telkom12@gmail.com>
2022-11-22 00:15:19 +0800

    update import path

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/orbitcontrols.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/gltfloader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const scene = new THREE.Scene()
 
